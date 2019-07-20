da2ef6d27e23c392d1714651d54ba7b4e0e49341
Daniel031000 <52997667+Daniel031000@users.noreply.github.com>
2019-07-20 13:45:58 +0200

    poasjfpdi

---
+++ game_logic.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+class pacman(self):
+
+    def __init__(self):
+        self.movement_direction = [0,0]
+        self.position = [100,100]
+        self.movement_speed = 5
+
+
+    def up (self):
+    def down(self):
+    def left(self):
+    def right(self):
+        
