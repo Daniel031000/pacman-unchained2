98b37f867a1067253424862de5ecbd79959f14ce
Daniel031000 <52997667+Daniel031000@users.noreply.github.com>
2019-07-20 15:30:09 +0200

    lug.iug

--- game_logic.py
+++ game_logic.py
@@ -5,31 +5,42 @@ import global_variables as gv
 class pacman:
 
     def __init__(self):
+        x = 100
+        y = 100
         self.movement_direction = [0, 0]
-        self.position = [100, 100]
+        self.position = [x, y]
         self.movement_speed = 5
 
     # define every direction
     def up(self):
-        for event in gv.pygame_events:
-            if gv.pygame_events == pygame.K_w:
-                self.movement_direction[1] = -1
-                self.movement_direction[0] = 0
-
+        try:
+            for event in gv.pygame_events:
+                if gv.pygame_events == pygame.K_w:
+                    self.movement_direction[1] = -1
+                    self.movement_direction[0] = 0
+                    y = y-1
+                    gv.pacman_position = [x, y]
+        except:
+            print("Ed SHeerans's fault")
+            pass
     def down(self):
         for event in gv.pygame_events:
             if gv.pygame_events == pygame.K_s:
                 self.movement_direction[1] = 1
                 self.movement_direction[0] = 1
-
+                y = y+1
+                gv.pacman_position = [x, y ]
     def left(self):
         for event in gv.pygame_events:
             if gv.pygame_events == pygame.K_a:
                 self.movement_direction[0] = -1
                 self.movement_direction[1] = 0
-
+                x = x-1
+                gv.pygame_events = [x, y]
     def right(self):
         for event in gv.pygame_events:
             if gv.pygame_events == pygame.K_d:
                 self.movement_direction[0] = 1
                 self.movement_direction[1] = 0
+                x = x+1
+                gv.pacman_position = [x, y]
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ LEVEL_PATHS = ["Graphics/levels/level1.png", "Graphics/levels/level2.png", "Grap
 
 screen = pygame.display.set_mode(gv.screensize)
 pygame.display.set_caption("Daniel`s PacMan")
+clock = pygame.time.Clock()
+
 # call pay_man
 pacman = gl.pacman()
 
@@ -19,12 +21,14 @@ pacman = gl.pacman()
 while 1:
     # event loop
     gv.pygame_events = pygame.event
-    for event in gv.pygame_events.get():
+    
+    for event in gv.pygame_events:
         if event.type == pygame.QUIT:
             sys.exit()
 
     # game logic loop
-
+    pacman.up()
+    print(gv.pacman_position)
 
     # animation
     ani.blit_level(LEVEL_PATHS[0], screen)
@@ -33,6 +37,7 @@ while 1:
     if gv.pacman_ani_counter >= 5:
         gv.pacman_ani_counter = 0
 
-    ani.blit_player()
+#    ani.blit_player(screen)
 
     pygame.display.flip()
+    clock.tick(30)
