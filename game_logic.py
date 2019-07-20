a4fcc9b081d06bec383938fb94ecbb139b215503
Daniel031000 <52997667+Daniel031000@users.noreply.github.com>
2019-07-20 14:03:23 +0200

    hozöizöouzöoz

--- game_logic.py
+++ game_logic.py
@@ -1,15 +1,35 @@
 import pygame
+import global_variables as gv
 
 
 class pacman(self):
 
     def __init__(self):
-        self.movement_direction = [0,0]
-        self.position = [100,100]
+        self.movement_direction = [0, 0]
+        self.position = [100, 100]
         self.movement_speed = 5
 
 
     def up (self):
+        for event in gv.pygame_events:
+            if gv.pygame_events == pygame.K_w:
+                self.movement_direction[1] = -1
+                self.movement_direction[0] = 0
+
     def down(self):
+        for event in gv.pygame_events:
+            if gv.pygame_events == pygame.K_s:
+                self.movement_direction[1] = 1
+                self.movement_direction[0] = 1
+
     def left(self):
+        for event in gv.pygame_events:
+            if gv.pygame_events == pygame.K_a:
+                self.movement_direction[0] = -1
+                self.movement_direction[1] = 0
+
     def right(self):
+        for event in gv.pygame_events:
+            if gv.pygame_events == pygame.K_d:
+                self.movement_direction[0] = 1
+                self.movement_direction[1] = 0
