396ef4e1c92f789b3541efb169308fe47b8e70a6
Daniel031000 <52997667+Daniel031000@users.noreply.github.com>
2019-07-21 14:15:31 +0200

    sdefwef

--- enemy.py
+++ enemy.py
@@ -1,21 +1,25 @@
 # ghost moving randomly
 import random
 
-class Ghost:
+class Ghost_random:
     def __init__(self):
         self.movement_direction = [0,0]
         self.movement_speed = 5
+        self.position = [x,y]
 
     def moving_randomly(self):
         self.movement_direction[random(0, 1)] = random(-1, 1)
         if self.movement_direction[0] == 1:
-
+            self.movementDirection[1] = 0
+            self.position = self.position[self.position[0] + self.movement_speed, self.position[1]]
         elif self.movement_direction[0] == -1:
-
+            self.movementDirection[1] = 0
+            self.position = self.position[self.position[0] - self.movement_speed, self.position[1]]
         elif self.movement_direction[1] == 1:
-
+            self.movementDirection[0] = 0
+            self.position = self.position[self.position[0], self.position[1] + self.movement_speed]
         elif self.movement_direction[1] == -1:
-
-
+            self.movementDirection[0] = 0
+            self.position = self.position[self.position[0], self.position[1] - self.movement_speed]
         else:
             break
