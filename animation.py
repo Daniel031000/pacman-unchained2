869b9d4c7ecc052b742d9cddca60a981b5743a06
QuantumFillet <xajoc8@gmail.com>
2019-07-21 12:53:18 +0200

    changed comment

--- animation.py
+++ animation.py
@@ -2,14 +2,16 @@ import pygame
 import global_variables as gv
 
 
-PACMAN_IMAGES = ["Graphics/pacman/pacman0.png"]
+PACMAN_IMAGES = ["Graphics/pacman/pacman0.png", "Graphics/pacman/pacman1.png", "Graphics/pacman/pacman2.png", "Graphics/pacman/pacman3.png"]
 
 
+# draws the level
 def blit_level(path, surface):
     level = pygame.image.load(path)
     surface.blit(level, level.get_rect())
 
 
+# takes the position of pacman and draws him to the screen
 def blit_player(surface):
 
     surface.blit()
