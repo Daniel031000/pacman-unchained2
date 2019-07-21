6e0246a2a7bd414cab3d70247443ec73abc02c86
Daniel031000 <52997667+Daniel031000@users.noreply.github.com>
2019-07-21 13:04:30 +0200

    wes0ifjüoiwd

--- game_logic.py
+++ game_logic.py
@@ -7,40 +7,29 @@ class pacman:
     def __init__(self):
         x = 100
         y = 100
-        self.movement_direction = [0, 0]
+
         self.position = [x, y]
         self.movement_speed = 5
 
     # define every direction
     def up(self):
-        try:
-            for event in gv.pygame_events:
-                if gv.pygame_events == pygame.K_w:
-                    self.movement_direction[1] = -1
-                    self.movement_direction[0] = 0
-                    y = y-1
-                    gv.pacman_position = [x, y]
-        except:
-            print("Ed SHeerans's fault")
-            pass
+        self.movement_direction[1] = -1
+        self.movement_direction[0] = 0
+        # update position when moving   
+        self.position = [self.position[0], self.position[1] - self.movement_speed]
+        gv.pacman_position = self.position
     def down(self):
-        for event in gv.pygame_events:
-            if gv.pygame_events == pygame.K_s:
-                self.movement_direction[1] = 1
-                self.movement_direction[0] = 1
-                y = y+1
-                gv.pacman_position = [x, y ]
+        self.movement_direction[1] = 1
+        self.movement_direction[0] = 1
+        self.position = [self.position[0], self.position[1] + self.movement_speed]
+        gv.pacman_position = self.position
     def left(self):
-        for event in gv.pygame_events:
-            if gv.pygame_events == pygame.K_a:
-                self.movement_direction[0] = -1
-                self.movement_direction[1] = 0
-                x = x-1
-                gv.pygame_events = [x, y]
+        self.movement_direction[0] = -1
+        self.movement_direction[1] = 0
+        self.position = [self.position[0] - self.movement_speed, self.position[1]]
+        gv.pacman_position= self.position
     def right(self):
-        for event in gv.pygame_events:
-            if gv.pygame_events == pygame.K_d:
-                self.movement_direction[0] = 1
-                self.movement_direction[1] = 0
-                x = x+1
-                gv.pacman_position = [x, y]
+        self.movement_direction[0] = 1
+        self.movement_direction[1] = 0
+        self.position = [self.position[0] + self.movement_speed, self.position[1]]
+        gv.pacman_position = self.position
